8579ee5880876f04e59dce0d0a7dbe3c4f8dd441
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-07-12 21:18:59 +0300

    editeTo Work With RefreshToken

--- src/js/refresh-token.js
+++ src/js/refresh-token.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://zaidrealstate.tryasp.net";
+const baseUrl = "https://localhost:7134";
 
 export async function refreshToken() {
   const oldToken = localStorage.getItem("token");
@@ -11,7 +11,8 @@ export async function refreshToken() {
 
   try {
     const response = await fetch(`${baseUrl}/api/Auth/Refresh`, {
-      method: "GET", // often POST is expected even with no body
+      method: "GET",
+      credentials: "include" 
     });
 
     const result = await response.json();
